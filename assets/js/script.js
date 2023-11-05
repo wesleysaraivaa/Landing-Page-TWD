253e310409ef6df2c3327771fde6bb55920ab95d
wesleysaraivaa <wesley-saraiva@outlook.com>
2023-11-05 00:32:11 -0300

    (feat:script.js): inclusao ao clicar no link do menu o menu sair da tela

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,13 @@ const hamburguer = document.querySelector(".hamburguer");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburguer.addEventListener("click", () => {
+
     hamburguer.classList.toggle('active');
     navMenu.classList.toggle('active');
+
 })
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburguer.classList.remove("acitve");
+    navMenu.classList.remove('active');
+}))
